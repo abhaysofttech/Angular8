6bbf05ceb57c47557c1e0029c0d89a2b8b9ada4d
Abhay Narnaware <abnarnaw@cisco.com>
2019-07-08 12:45:07 +0530

    Update the process by adding service to get the recipe & Shopping list

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,10 @@
+import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>();
+
+
     private recipes: Recipe[] = [
         new Recipe('A Test Recipe', 'This is a Simply test', 'https://www.goya.com/media/5284/no-bake-icon.png'),
         new Recipe('A Test Recipe Test', 'This is a Simply test Again', 'http://recipeworldapp.com/assets/images/app-store-265x265.png')
