7e8d119258f32b3cbea826f355c1a69417419e16
Abhay Narnaware <abnarnaw@cisco.com>
2019-07-09 18:13:05 +0530

    Passing Query Parameters and Fragments

--- routing-start/src/app/users/user/user.component.ts
+++ routing-start/src/app/users/user/user.component.ts
@@ -9,7 +9,7 @@ import { Subscription } from 'rxjs/Subscription';
 })
 export class UserComponent implements OnInit, OnDestroy {
   user: { id: number, name: string };
-  paramsSubscription : Subscription 
+  paramsSubscription : Subscription; 
 
   constructor(private route: ActivatedRoute) { }
 
@@ -18,12 +18,16 @@ export class UserComponent implements OnInit, OnDestroy {
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name'],
     };
-    this.route.params.subscribe(
+    this.paramsSubscription = this.route.params.subscribe(
       (params: Params) => {
         this.user.id = params['id'];
         this.user.name = params['name'];
       }
     );
+  }
+
+  ngOnDestroy(){
+    this.paramsSubscription.unsubscribe(); // adding unsubscribe the parame subcription
   }
 
 }
